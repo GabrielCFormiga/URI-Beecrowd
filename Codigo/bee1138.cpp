3cff82ffd5770887c11a6c6e7a246332801995d4
Gabriel Campelo <gabriel.campfor@gmail.com>
2024-02-15 16:28:13 -0300

    Update bee1338.cpp

--- Codigo/bee1138.cpp
+++ Codigo/bee1138.cpp
@@ -1,3 +1,4 @@
+// NOT SOLVED YET
 #include <bits/stdc++.h>
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
 #define endl '\n'
@@ -8,11 +9,25 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long llu;
 
-int main(){ 
-    int a, b;
-    while(1){
-        cin >> a >> b;
+int count(int n, int i){
+    int result = 0, pot = 1;
+    while(n > 0){
+        int dig = n % 10;
+        n / 10;
+        
+
+
+
+        pot *= 10;
     }
-    
-    return 0;
+}
+
+int main(){
+    int a, b;
+    while(cin >> a >> b && a != 0){
+        for(int i = 0; i < 10; i++){
+            cout << count(b, i) - count(a - 1, i);
+            i == 9 ? cout << endl : cout << ' ';
+        }
+    }
 }
