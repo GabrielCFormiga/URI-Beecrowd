7c5e2047aa8618431fc1220b956349e06fb6d048
Gabriel Campelo <gabriel.campfor@gmail.com>
2024-07-27 00:12:50 -0300

    Add 2146 - Senha

---
+++ Codigo/1 - Beginner/bee2146.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pv push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+int main() {
+    int n;
+    while (cin >> n) {
+        cout << n - 1 << endl;
+    }
+    
+    return 0;
+};
