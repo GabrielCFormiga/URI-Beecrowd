dbd703c7a8eb62b854a5368f8c6cc6b120c64dea
GabrielCFormiga <gabriel.campfor@gmail.com>
2024-08-13 13:43:17 -0300

    Add 2172 - Evento

---
+++ Codigo/1 - Iniciante/src/2172.cpp
@@ -0,0 +1,29 @@
+/**
+* @file 2172.cpp
+*
+* Created on 2024-08-13 at 13:39:55
+* @author GabrielCampelo
+*/
+
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() { _
+    ll x, m;
+    while(cin >> x >> m && x != 0) {
+        cout << x * m << endl;
+    } 
+    
+    return 0;
+};
