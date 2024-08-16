1d4b05fb8ce2e1ee1afcbc14eb2896b69705b7a0
GabrielCFormiga <gabriel.campfor@gmail.com>
2024-08-15 23:16:39 -0300

    Add 2483 - Feliz Nataaal!

---
+++ Codigo/1 - Iniciante/src/2483.cpp
@@ -0,0 +1,28 @@
+/**
+* @file 2483.cpp
+*
+* Created on 2024-08-15 at 23:12:56
+* @author GabrielCampelo
+*/
+
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() { _
+    int n;
+    cin >> n;
+    cout << "Feliz n";
+    for (int i = 0; i < n; i++) cout << 'a';
+    cout << "l!\n";
+};
