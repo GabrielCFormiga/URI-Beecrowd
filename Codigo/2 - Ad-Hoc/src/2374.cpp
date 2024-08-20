f3d3f54b9ba4af07d9a9ad66fb47c23b9d412ed9
GabrielCFormiga <gabriel.campfor@gmail.com>
2024-08-20 01:25:38 -0300

    Add 2374 - Pneu

---
+++ Codigo/2 - Ad-Hoc/src/2374.cpp
@@ -0,0 +1,28 @@
+/**
+* @file 2374.cpp
+*
+* Created on 2024-08-20 at 01:24:43
+* @author GabrielCampelo
+*/
+
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() { _
+    int n, m;
+    cin >> n >> m;
+    cout << n - m << endl;
+    
+    return 0;
+}
