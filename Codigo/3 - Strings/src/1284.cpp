2057c4d331bd02c71b7981e2695f2f345d8d9f45
GabrielCFormiga <gabriel.campfor@gmail.com>
2024-08-21 00:28:51 -0300

    Add 1284 - Digitando no Telefone Celular

---
+++ Codigo/3 - Strings/src/1284.cpp
@@ -0,0 +1,101 @@
+/**
+* @file 1284.cpp
+* Aho-Corasick
+*
+* Created on 2024-08-20 at 20:33:25
+* @author GabrielCampelo
+*/
+
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+struct No {
+    int proxm[26];
+    bool terminacao;
+    int palavras; 
+
+    No(int palavras) {
+        terminacao = false;
+        this->palavras = palavras;
+        fill(begin(proxm), end(proxm), -1);
+    }
+
+    No() {
+        terminacao = false;
+        palavras = 0;
+        fill(begin(proxm), end(proxm), -1);
+    } 
+};
+
+vector<No> trie;
+
+void add_string(string const &s) {
+    int v = 0;
+    for (char ch : s) {
+        int c = ch - 'a';
+        if (trie[v].proxm[c] == -1) {
+            trie[v].proxm[c] = trie.size();
+            trie.push_back(No(trie[v].palavras));
+        }
+        trie[v].palavras++;
+        v = trie[v].proxm[c];
+    }
+    trie[v].palavras++;
+    trie[v].terminacao = true;
+}
+
+int cnt;
+void busca(int teclas, int v) {
+    if (trie[v].palavras == 1) {
+        cnt += teclas;
+        return;
+    } else if (trie[v].terminacao) {
+        cnt += teclas;
+    }
+
+    vector<int> filhos;
+    for (int i = 0; i < 26; i++) {
+        if (trie[v].proxm[i] != -1) filhos.pb(trie[v].proxm[i]);
+    }
+
+    if (filhos.size() == 1) {
+        if (trie[v].terminacao) busca(teclas + 1, filhos[0]);
+        else busca(teclas, filhos[0]);
+    } else {
+        for (int i = 0; i < filhos.size(); i++) busca(teclas + 1, filhos[i]);
+    }
+}
+
+int main() { _
+    int n;
+    string s;
+    while (cin >> n) {
+        trie.clear();
+        trie.emplace_back();
+
+        for (int i = 0; i < n; i++) {
+            cin >> s;
+            add_string(s);
+        }
+
+        cnt = 0;
+        for (int i = 0; i < 26; i++) {
+            if (trie[0].proxm[i] != -1) busca(1, trie[0].proxm[i]);
+        }
+
+        cout << fixed << setprecision(2) << (float) cnt / n << endl;
+    } 
+    
+    return 0;
+}
