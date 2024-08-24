64931b8dbacc167966f87fd5eb5c1fcef264cb9d
GabrielCFormiga <gabriel.campfor@gmail.com>
2024-08-24 19:29:38 -0300

    Add 2510 - Batmain

---
+++ Codigo/1 - Iniciante/src/2510.cpp
@@ -0,0 +1,32 @@
+/**
+* @file 2510.cpp
+*
+* Created on 2024-08-24 at 19:25:46
+* @author GabrielCampelo
+*/
+
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() { _
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        string s;
+        cin >> s;
+        cout << 'Y' << endl; // "todos os seus vilões haviam sido capturados pelo Batmain"
+    } 
+    
+    return 0;
+}
