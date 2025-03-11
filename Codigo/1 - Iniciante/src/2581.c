935eaf46a4056ede5fe5c32c8c499c5d071e7f70
GabrielCFormiga <gabriel.campfor@gmail.com>
2025-03-10 21:22:15 -0300

    Add 2581 - I am Toorg!

---
+++ Codigo/1 - Iniciante/src/2581.c
@@ -0,0 +1,31 @@
+/**
+* @file 2581.c
+*
+* Created on 2025-03-10 at 21:19:06
+* @author GabrielCampelo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define INF 0x3f3f3f3f
+#define LINF 0x3f3f3f3f3f3f3f3fLL
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main() {
+    int n;
+    char line[100];
+
+    scanf("%d%*c", &n);
+
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+        printf("I am Toorg!\n");
+    }
+    
+    return 0;
+}
---
+++ Codigo/1 - Iniciante/src/2581.cpp
@@ -0,0 +1,32 @@
+/**
+* @file 2581.cpp
+*
+* Created on 2025-03-10 at 21:14:22
+* @author GabrielCampelo
+*/
+
+#include <bits/stdc++.h>
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long llu;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+int main() { _
+    int n;
+    cin >> n;
+    string line;
+    cin.ignore();
+    while (getline(cin, line)) {
+        cout << "I am Toorg!" << endl;
+    }
+    
+    return 0;
+}
